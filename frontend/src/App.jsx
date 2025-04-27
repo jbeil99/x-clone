2ffbc304e1864ec9af78d5ac08b8b268b4f8e000
jbeil <hussin25soli@gmail.com>
2025-04-28 02:39:59 +0300

    solve merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,10 @@ import MainPage from "./pages/main-page/Home"
 import Layout from "./components/layout"
 // import Register from "./pages/Register"
 import PrivateRoute from "./components/guards/PrivateRoute";
-<<<<<<< HEAD
 import ActivateAccount from "./pages/auth/ActivationPage";
-=======
 import Explore from "./pages/main-page/Explore";
 
 
->>>>>>> home-page
 
 function App() {
 
@@ -20,47 +17,28 @@ function App() {
     <BrowserRouter>
       <ToastContainer />
       <Routes>
-<<<<<<< HEAD
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/activate/:uid/:token" element={<ActivateAccount />} />
 
-        {/* <Route path="/register" element={<Register />} /> */}
         <Route element={<PrivateRoute />}>
           <Route path="/" element={
             <Layout>
               <MainPage />
             </Layout>
           } />
-          <Route path="/:username" element={
+          <Route path="/explore" element={
+            <Layout>
+              <Explore />
+            </Layout>
+          } />
+          <Route path=":username" element={
             <Layout>
               <UserProfile />
             </Layout>
           } />
         </Route>
       </Routes>
-
-=======
-  <Route path="/login" element={<AuthPage />} />
-  <Route element={<PrivateRoute />}>
-    <Route path="/" element={
-      <Layout>
-        <MainPage />
-      </Layout>
-    } />
-    <Route path="/explore" element={
-      <Layout>
-        <Explore />
-      </Layout>
-    } />
-    <Route path=":username" element={
-      <Layout>
-        <UserProfile />
-      </Layout>
-    } />
-  </Route>
-</Routes>
->>>>>>> home-page
-    </BrowserRouter>
+    </BrowserRouter >
   )
 }
 
