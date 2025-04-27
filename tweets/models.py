18dd21ad45e692729c5532544337cc1da46207c9
jbeil <hussin25soli@gmail.com>
2025-04-27 19:10:22 +0300

    add: likes, retweets and replies to tweets model

--- tweets/models.py
+++ tweets/models.py
@@ -1,24 +1,51 @@
 from django.db import models
-from users.models import User
+from accounts.models import User
+
 
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=140)
     image = models.ImageField(blank=True, null=True)
-    liked = models.ManyToManyField(User, default=None, blank=True, related_name='liked')
-    retweeted = models.ManyToManyField(User, default=None, blank=True, related_name='retweeted')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ["-created_at"]
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, related_name="likes", on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, related_name="likes", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("liked", "tweet")
+
+    def __str__(self):
+        return f"{self.liked.username} liked {self.tweet}"
+
+
+class Retweet(models.Model):
+    retweeted = models.ForeignKey(
+        User, related_name="retweets", on_delete=models.CASCADE
+    )
+    tweet = models.ForeignKey(Tweet, related_name="retweets", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("retweeted", "tweet")
+
+    def __str__(self):
+        return f"{self.user.username} retweeted {self.tweet.content[:20]}"
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name='parent')
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name="parent")
     body = models.CharField(max_length=140)
+    parent = models.ForeignKey(
+        "self", null=True, blank=True, related_name="replies", on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at']
-
-
+        ordering = ["-created_at"]
